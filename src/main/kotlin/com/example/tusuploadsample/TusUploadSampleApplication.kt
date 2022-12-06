08921bef8e27eeb36c45aaaba818148435bccc89
shane <psh40963@naver.com>
2022-12-06 17:34:29 +0900

    init project

---
+++ src/main/kotlin/com/example/tusuploadsample/TusUploadSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.tusuploadsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TusUploadSampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<TusUploadSampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/tusuploadsample/TusUploadSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.tusuploadsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TusUploadSampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
